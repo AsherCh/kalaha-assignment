42d8307ba92ea9b3f531741063448018dbbacbe9
Chi Zhang <icswilliam2012@gmail.com>
2024-03-03 17:22:20 +0100

    Create Minmax.py

---
+++ Minmax.py
@@ -0,0 +1,14 @@
+import copy
+
+class Minmax:
+    def __init__(self,max_depth=5):
+        self.max_depth = max_depth
+
+    def alpha_beta():
+        pass
+    
+    def max_value():
+        pass
+    def min_value():
+        pass
+    

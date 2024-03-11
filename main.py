a71f6aa1c00d08bf4b347c87082e789b751bfd4d
Chi Zhang <icswilliam2012@gmail.com>
2024-03-11 08:43:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from Board import *
 
 def print_board(board):
     state = board.get_state()
-    print ("-------------Kalaha----------------")
-    print ("-------------By Group 39-----------")
 
-    print ("You will play this game with the computer.Please pick up your stones from your side first")
     print("==========================================")
     print("Computer", end=" ");print("|", end="  "); print(" |",end=" ");print(*state[0][0:-1],sep=" | ",end="");print(" |",end="  ");print(" |")
     print("        ", end=" ");print("|", end=" "); print(state[0][-1],end=" ");print("|",end="");print("----------------------",end="");print(" |",end=" ");print(state[1][-1],end=" ");print("|")
@@ -13,11 +10,38 @@ def print_board(board):
     print("==========================================")
 
 if __name__ == "__main__":
+    print ("-------------Kalaha----------------")
+    print ("-------------By Group 39-----------")
+    print ("You will play this game with the computer.Please pick up your stones from your side first")
     # Play the game
-    HUMAN_PLAYER = 0
-    COMPUTER_PLAYER = 1
-    board = Board()
+    HUMAN = 1
+    COMPUTER = 0
+    MAX_DEPTH = 2
+    #Human as the first player
+    board = Board(HUMAN)
+    #print the initial board
     print_board (board)
-    print ("Please pick up your stones from your side:")
-    pit_index = int(input())-1
-    board.take_stones(pit_index)
+    #Set the board status as the node
+    index =0
+    while index < 7:
+        #Human input the first move
+        current_player=board.get_turn()
+        print (current_player," Human,please pick up your stones from your side:")
+        pit_index = int(input())-1
+        board.take_stones(pit_index)
+        print_board (board)
+        #Change the player to the computer
+        board.reverse_player()
+        current_player=board.get_turn()
+        print (current_player," Computer's best move")
+        root_node = Node(board)
+        tree = Tree(root_node,MAX_DEPTH)
+        tree.build(tree.get_root(),0)
+
+        print ("                    ")
+        print("==========================================")
+        #Change the player to the Human
+        board.reverse_player()
+        index +=1
+
+    print ("Game is over")

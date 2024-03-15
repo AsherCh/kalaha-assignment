eed214ff76dd8a134aff88a376196edc01c50b96
Chi Zhang <icswilliam2012@gmail.com>
2024-03-15 11:34:29 +0100

    Update Evaluation_function.py

--- Evaluation_function.py
+++ Evaluation_function.py
@@ -3,13 +3,9 @@ def evaluation_fuction(board):
     state = board.get_state()
     utility_value = 0
     # the score of each player = the number of stealing stones
-    human_score = sum(state[0])
-    computer_score = sum(state[1])
+    human_score = sum(state[1])
+    computer_score = sum(state[0])
     #utility = player1_score - player2_score
     utility_value = computer_score - human_score
     return utility_value
 
-
-def check_utility():
-    #check the player utilty 
-    pass

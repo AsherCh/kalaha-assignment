3a1e1e78c8ddddd8c462f5ab8eb072fed0e58787
Chi Zhang <icswilliam2012@gmail.com>
2024-03-16 19:50:08 +0100

    Update main.py
    1. Add more evaluation_fuction()
    2. Change the AI vs AI mode
    3. Add the continue playing features

--- main.py
+++ main.py
@@ -24,29 +24,89 @@ if __name__ == "__main__":
         children = node.get_children()
         return children[a]
 
-    minmax = Minmax(evaluation_fuction, find_children, MAX_DEPTH)
+    ##  evaluation_function2 and evaluation_function3 are better
+    #minmax = Minmax(evaluation_fuction, find_children, MAX_DEPTH)
+    minmax = Minmax(evaluation_function2, find_children, MAX_DEPTH)
+    #minmax = Minmax(evaluation_function3, find_children, MAX_DEPTH)  
+    #minmax = Minmax(evaluation_function4, find_children, MAX_DEPTH)
+
     game_end = False
     while not game_end:
-        game_end = board.terminate()
+        '''
         # Human input the first move
         current_player = board.get_turn()
         print(current_player, " Human,please pick up your stones from your side:")
         pit_index = board.get_user_move()
         board.take_stones(pit_index)
         board.print_board()
+        
+        '''
+        '''
+        root_node = Node(board)
+        tree = Tree(root_node, MAX_DEPTH)
+        tree.build(tree.get_root(), 0)
+        #print ("First AI tree",tree.get_root().get_data().get_state())
+        utility, move_i = minmax.alpha_beta(tree)
+        print("First AI Best Move: Utility:", utility, "Best_Move:", move_i+1)
+        board.take_stones(move_i)
+        board.print_board()
+
+        
+        # """ AI player """
+       # """ Comment below lines to disable AI """
+        root_node = Node(board)
+        tree = Tree(root_node, MAX_DEPTH)
+        tree.build(tree.get_root(), 0)
+        utility, move_i = minmax.alpha_beta(tree)
+        print("Second AI Best Move: Utility:", utility, "Best_Move:", move_i+1)
+        board.take_stones(move_i)
+        board.print_board()
+
+        '''
+        root_node = Node(board)
+        tree = Tree(root_node, MAX_DEPTH)
+        tree.build(tree.get_root(), 0)
+        utility, move_i = minmax.alpha_beta(tree)
+        print("First AI Best Move: Utility:", utility, "Best_Move:", move_i+1)
+        response = board.take_stones(move_i)
+        board.print_board()
+        if response == "continue":
+            root_node = Node(board)
+            tree = Tree(root_node, MAX_DEPTH)
+            tree.build(tree.get_root(), 0)
+            utility, move_i = minmax.alpha_beta(tree)
+            print("Continue First AI Best Move: Utility:", utility, "Best_Move:", move_i+1)
+            response = board.take_stones(move_i)
+            board.print_board()
+        
+        game_end = board.terminate()
+        if game_end:
+            break
 
         board.reverse_player()
+       
+        root_node = Node(board)
+        tree = Tree(root_node, MAX_DEPTH)
+        tree.build(tree.get_root(), 0)
+        utility, move_i = minmax.alpha_beta(tree)
+        print("Second AI Best Move: Utility:", utility, "Best_Move:", move_i+1)
+        response = board.take_stones(move_i)
+        board.print_board()
+        if response == "continue":
+            root_node = Node(board)
+            tree = Tree(root_node, MAX_DEPTH)
+            tree.build(tree.get_root(), 0)
+            utility, move_i = minmax.alpha_beta(tree)
+            print("Continue Second AI Best Move: Utility:", utility, "Best_Move:", move_i+1)
+            response = board.take_stones(move_i)
+            board.print_board()
 
-        """ AI player """
-        """ Comment below lines to disable AI """
-        # root_node = Node(board)
-        # tree = Tree(root_node, MAX_DEPTH)
-        # tree.build(tree.get_root(), 0)
-        # utility, move_i = minmax.alpha_beta(tree)
-        # print("AI Best Move: Utility:", utility, "Best_Move:", move_i)
-        # board.take_stones(move_i)
-        # board.print_board()
+        game_end = board.terminate()
+        if game_end:
+            break
 
+        '''
+        
         """---------Player 2----------------"""
         """ Comment below lines to disable player 2 """
         print("Player 2 choose your move")
@@ -54,8 +114,9 @@ if __name__ == "__main__":
         board.take_stones(pit_index_p2)
         board.print_board()
         """-------------end-----------------"""
-
+       '''
         board.reverse_player()
 
-    print("Game is over")
+  
     board.get_winner()
+    print("Game is over")

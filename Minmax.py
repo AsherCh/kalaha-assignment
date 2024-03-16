ca0831e3d2c028651e118d1ba9dc237b84c24ac6
Chi Zhang <icswilliam2012@gmail.com>
2024-03-16 19:51:13 +0100

    Update Minmax.py
    1. Add the leaf node checking

--- Minmax.py
+++ Minmax.py
@@ -14,16 +14,19 @@ class Minmax:
     def max_value(self,node,a,b,depth):
         if depth >= self.max_depth:
             value = node.get_utility(self.eval_func)
-            #print ("Utility:",value)
+            #print ("max_value Utility:",value)
             return value,0
         
         # alpha
         v = float('-inf')
         best_move=-1
 
-        for i in node.get_children().keys():
-            child_node = self.find_child(node,i)
+        if len(node.get_children()) != 0:
+            for i in node.get_children().keys():
+                child_node = self.find_child(node,i)
+            #print ("max_value node.get_children().key():",node.get_children().keys())
             #print ("max_value: ",child_node.get_data().get_state())
+
             v = max(v,self.min_value(child_node,a,b,depth+1)[0])
             # cut off
             if v >= b:
@@ -33,19 +36,22 @@ class Minmax:
                 best_move = i
 
             a = max(a,v)
+        else:
+            return node.get_utility(self.eval_func),0
         return v,best_move
 
     def min_value(self,node,a,b,depth):
         if depth >= self.max_depth:
             value = node.get_utility(self.eval_func)
-            #print ("Utility:",value)
+            #print ("min_value Utility:",value)
             return value,0
         
         v = float('inf')
         best_move=-1
-
-        for i in node.get_children().keys():
-            child_node = self.find_child(node,i)
+        #print ("min_value node.get_children().key():",node.get_children().keys())
+        if len(node.get_children()) != 0:
+            for i in node.get_children().keys():
+                child_node = self.find_child(node,i)
             #print ("min_value: ",child_node.get_data().get_state())
             v = min(v,self.max_value(child_node,a,b,depth+1)[0])
             # cut off
@@ -56,4 +62,6 @@ class Minmax:
                 best_move = i
 
             b = min(b,v)
+        else:
+            return node.get_utility(self.eval_func),0
         return v,best_move
